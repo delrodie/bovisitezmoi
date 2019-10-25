1e1d351a6c6062efffdeed62a1d2934ae4a75b4e
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-10-25 15:03:48 +0000

    Gestion des modes

---
+++ src/AppBundle/Controller/ModeController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Mode;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Mode controller.
+ *
+ * @Route("backend/mode")
+ */
+class ModeController extends Controller
+{
+    /**
+     * Lists all mode entities.
+     *
+     * @Route("/", name="backend_mode_index")
+     * @Method({"GET", "POST"})
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $modes = $em->getRepository('AppBundle:Mode')->findAll();
+        $mode = new Mode();
+        $form = $this->createForm('AppBundle\Form\ModeType', $mode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($mode);
+            $em->flush();
+
+            return $this->redirectToRoute('backend_mode_index');
+        }
+
+        return $this->render('mode/index.html.twig', array(
+            'modes' => $modes,
+            'mode' => $mode,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a new mode entity.
+     *
+     * @Route("/new", name="backend_mode_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $mode = new Mode();
+        $form = $this->createForm('AppBundle\Form\ModeType', $mode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($mode);
+            $em->flush();
+
+            return $this->redirectToRoute('backend_mode_show', array('id' => $mode->getId()));
+        }
+
+        return $this->render('mode/new.html.twig', array(
+            'mode' => $mode,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a mode entity.
+     *
+     * @Route("/{id}", name="backend_mode_show")
+     * @Method("GET")
+     */
+    public function showAction(Mode $mode)
+    {
+        $deleteForm = $this->createDeleteForm($mode);
+
+        return $this->render('mode/show.html.twig', array(
+            'mode' => $mode,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing mode entity.
+     *
+     * @Route("/{slug}/edit", name="backend_mode_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Mode $mode)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $modes = $em->getRepository('AppBundle:Mode')->findAll();
+        $deleteForm = $this->createDeleteForm($mode);
+        $editForm = $this->createForm('AppBundle\Form\ModeType', $mode);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('backend_mode_edit', array('id' => $mode->getId()));
+        }
+
+        return $this->render('mode/edit.html.twig', array(
+            'mode' => $mode,
+            'modes' => $modes,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a mode entity.
+     *
+     * @Route("/{id}", name="backend_mode_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Mode $mode)
+    {
+        $form = $this->createDeleteForm($mode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($mode);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('backend_mode_index');
+    }
+
+    /**
+     * Creates a form to delete a mode entity.
+     *
+     * @param Mode $mode The mode entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Mode $mode)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('backend_mode_delete', array('id' => $mode->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
