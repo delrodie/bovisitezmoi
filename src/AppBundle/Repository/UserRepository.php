d7371ea914cce5f9b950a892a66e8cf6bb01b3b3
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-11-13 15:48:24 +0000

    Frontend liste des users

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository
+{
+    /**
+     * UserController:indexAction
+     * @return array
+     */
+    public function findList()
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.username <> :user')
+            ->orderBy('u.username', 'ASC')
+            ->setParameter('user', 'delrodie')
+            ->getQuery()->getResult()
+            ;
+    }
+}
